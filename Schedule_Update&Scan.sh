d1f4e04430ee78141517585b9b151ba5b41b0725
TheRootRouteway <162377419+TheRootRouteway@users.noreply.github.com>
2024-03-10 21:34:28 +0100

    Update Schedule_Update&Scan.sh

--- Schedule_Update&Scan.sh
+++ Schedule_Update&Scan.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-# Existing CRON job for running mdatp scan
+# Delete the current user's crontab contents
+crontab -r
+# CRON job for running mdatp scan
 CRON_JOB1="CRON_TZ=Europe/Paris\n0 11 * * Wed /bin/mdatp scan quick > ~/mdatp_cron_scan.log"
 
-# New CRON job to upgrade mdatp on Tuesdays before or on the 15th of the month
+# CRON job to upgrade mdatp on Tuesdays before or on the 15th of the month
 CRON_JOB2="0 11 * * Tue [ \$(date +\\%d) -le 15 ] && apt-get install --only-upgrade mdatp > ~/mdatp_cron_prod_update.log"
+# CRON job to update definitions
 CRON_JOB3="0 10 * * * /bin/mdatp definitions update > ~/mdatp_cron_def_update"
+
 # Combine both CRON jobs
 CRON_JOBS="$CRON_JOB1\n$CRON_JOB2\n$CRON_JOB3"
 
